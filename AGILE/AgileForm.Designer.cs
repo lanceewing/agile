c2c240482d11b85c8f1039304dd593420cb3f825
Lance Ewing <lance.ewing@gmail.com>
2020-07-03 22:28:44 +0100

    Updated window title to AGILE.

--- AGILE/AgileForm.Designer.cs
+++ AGILE/AgileForm.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            //
+            // AgileForm
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(640, 400);
+            this.Name = "AgileForm";
+            this.Text = "AGILE";
+            this.ResumeLayout(false);
         }
 
         #endregion
--- AGILE/Program.cs
+++ AGILE/Program.cs
@@ -14,7 +14,7 @@ namespace AGILE
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new AgileForm(new AGI.Game("E:\\games\\AGIStudio138b1\\savegame")));
-            //Application.Run(new AgileForm(new AGI.Game("E:\\games\\kq1")));
+            Application.Run(new AgileForm(new AGI.Game("C:\\games\\kq1")));
             //Application.Run(new AgileForm(new AGI.Game("E:\\games\\kq2")));
             //Application.Run(new AgileForm(new AGI.Game("E:\\games\\kq3")));
             //Application.Run(new AgileForm(n/ew AGI.Game("E:\\games\\sq1")));
@@ -23,7 +23,7 @@ namespace AGILE
             //Application.Run(new AgileForm(new AGI.Game("E:\\games\\mumg")));
             //Application.Run(new AgileForm(new AGI.Game("E:\\games\\ddp")));
             //Application.Run(new AgileForm(new AGI.Game("E:\\games\\bc")));
-            Application.Run(new AgileForm(new AGI.Game("C:\\games\\ruby")));
+            //Application.Run(new AgileForm(new AGI.Game("C:\\games\\ruby")));
 
             //Application.Run(new AgileForm(new AGI.Game("E:\\games\\gr")));
             //Application.Run(new AgileForm(new AGI.Game("E:\\games\\kq4agi")));
