ee6b7e8bc4cb2a0ddc96fa7fb7ec784e5467694f
ABranscom <abranscom@sierrahelp.com>
2022-11-02 12:03:03 -0400

    Update AgileForm.Designer.cs

--- AGILE/AgileForm.Designer.cs
+++ AGILE/AgileForm.Designer.cs
@@ -61,6 +61,13 @@ namespace AGILE
             this.cntxtMenuOpenUserConfig.Visible = false;
             this.cntxtMenuOpenUserConfig.Click += new System.EventHandler(this.cntxtMenuOpenUserConfig_Click);
             // 
+            // cntxtMenuOptions
+            // 
+            this.cntxtMenuOptions.Name = "cntxtMenuOptions";
+            this.cntxtMenuOptions.Size = new System.Drawing.Size(189, 22);
+            this.cntxtMenuOptions.Text = "Open &Options";
+            this.cntxtMenuOptions.Click += new System.EventHandler(this.cntxtMenuOptions_Click);
+            // 
             // cntxtMenuFullScreen
             // 
             this.cntxtMenuFullScreen.CheckOnClick = true;
@@ -94,13 +101,6 @@ namespace AGILE
             this.cntxtMenuStretchMode.Size = new System.Drawing.Size(189, 22);
             this.cntxtMenuStretchMode.Text = "Stretch Mode";
             this.cntxtMenuStretchMode.Click += new System.EventHandler(this.cntxtMenuStretchMode_Click);
-            // 
-            // cntxtMenuOptions
-            // 
-            this.cntxtMenuOptions.Name = "cntxtMenuOptions";
-            this.cntxtMenuOptions.Size = new System.Drawing.Size(189, 22);
-            this.cntxtMenuOptions.Text = "Open &Options";
-            this.cntxtMenuOptions.Click += new System.EventHandler(this.cntxtMenuOptions_Click);
             // 
             // AgileForm
             // 
